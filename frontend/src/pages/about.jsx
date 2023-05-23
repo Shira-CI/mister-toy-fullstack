69d1f9b7870994cd385d8ef26bef58dcf8363573
Shira Cohen <shira140@gmail.com>
2023-05-23 20:37:10 +0300

    render update

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,6 +1,8 @@
+import { GoogleMap } from "../cmps/google-map";
 
 export function About() {
     return <section className="about">
         <h1>About Page</h1>
+        <GoogleMap/>
     </section>
 }
