af00c71fc5e46f8c321a093c043e0d8b468928cd
Shira Cohen <shira140@gmail.com>
2023-05-27 01:19:22 +0300

    mq toy preview+list

--- frontend/src/cmps/toy.list.jsx
+++ frontend/src/cmps/toy.list.jsx
@@ -2,17 +2,12 @@
 
 import { ToyPreview } from "./toy.preview";
 
-
-export function ToyList({ toys ,onRemoveToy}) {
+export function ToyList({ toys, onRemoveToy }) {
     return <ul className="toy-list">
-        {toys.map(toy => 
+        {toys.map(toy =>
 
-            <li className="toy-preview" key={toy._id}>
-                <ToyPreview toy={toy} />
-                <section>
-                    <button onClick={() => { onRemoveToy(toy._id) }}>x</button>
-                </section>
-
+            <li className="toy-preview-container" key={toy._id}>
+                <ToyPreview toy={toy} onRemoveToy={onRemoveToy} />
             </li>)}
     </ul>
 }
--- frontend/src/cmps/toy.preview.jsx
+++ frontend/src/cmps/toy.preview.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
 
-export function ToyPreview({ toy }) {
-    return <article>
-        <h4>{toy.title}</h4>
-        <img src= {toy.image} alt="" />
-       
-        <p>Price: <span>{toy.price}$</span></p>
-        {toy.inStock && <span>Toy available</span>}
+export function ToyPreview({ toy, onRemoveToy }) {
+    return (
+        <article className="toy-preview">
+            <Link className="details-a-link" to={`/toy/${toy._id}`}>
+            <h4>{toy.title}</h4>
+            <img src={toy.image} alt="" />
 
-        <br />
-        <Link to={`/toy/${toy._id}`}>Details</Link> |
-        <br />
-        <Link to={`/toy/edit/${toy._id}`}>Edit</Link>
-
-    </article>
+            <p>Price: <span>{toy.price}$</span></p>
+            {toy.inStock && <span>Toy available</span>}
+            </Link>
+            <section>
+                <button> <Link to={`/toy/edit/${toy._id}`}>Edit</Link> </button>
+                <button onClick={() => { onRemoveToy(toy._id) }}>Remove Toy</button>
+            </section>
+        </article>
+    )
 }
 
